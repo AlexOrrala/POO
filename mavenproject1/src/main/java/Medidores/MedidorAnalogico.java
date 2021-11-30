c13439f1c1b8f8acbe7bac0db3911a654e818f50
samu_ <samu_@DESKTOP-K5NQHKN>
2021-11-30 00:34:26 -0500

    Clase medidorAnalogico

---
+++ mavenproject1/src/main/java/Medidores/MedidorAnalogico.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Medidores;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author samu_
+ */
+public class MedidorAnalogico extends Medidor {
+    private LocalDateTime ultimamed;
+    private int Lecturaa;
+    public MedidorAnalogico(LocalDateTime ultimamed,int Lecturaa){
+        super();
+        this.ultimamed=ultimamed;
+        this.Lecturaa=Lecturaa;
+    }
+    public LocalDateTime getUltimamed(){
+        return ultimamed;
+    }
+    public int getLectuaraa(){
+        return Lecturaa;
+    }
+    public void setUltimamed(LocalDateTime ultimamed ){
+        this.ultimamed=ultimamed;
+    }
+    public void setLecturaa(int Lecturaa){
+        this.Lecturaa=Lecturaa;
+    }
+    
+    
+}
