beccbbec991b6e3d522385146d387198721a69b8
samu_ <samu_@DESKTOP-K5NQHKN>
2021-11-30 00:26:22 -0500

    Clase medidor(correccion de paquete)

---
+++ mavenproject1/src/main/java/Medidores/Medidor.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Medidores;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author samu_
+ */
+public abstract class Medidor {
+    private String codigo;
+    private static int numdeserie=000;
+    public Medidor(){
+        codigo="Med"+numdeserie;
+        numdeserie++;
+    }
+    public abstract double calcularValorPagar(LocalDateTime fechaAccion);
+    
+}
