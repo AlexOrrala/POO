6b51225aad670f07e9bf6b26503f671911c504d1
Alexorrala <91976823+AlexOrrala@users.noreply.github.com>
2021-12-02 15:52:19 -0500

    2

--- mavenproject1/src/main/java/Medidores/MedidorDigital.java
+++ mavenproject1/src/main/java/Medidores/MedidorDigital.java
@@ -5,13 +5,24 @@
 
 package Medidores;
 
+import Plan.PlanEnergia;
+import Plan.Provincias;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 /**
  * 
  * @author Sammy Guergachi <sguergachi at gmail.com>
  */
-public class MedidorDigital {
+public class MedidorDigital extends Medidor{
     private LocalDateTime FechaToma;
     private double kilovatios;
+    public MedidorDigital(String codigo,PlanEnergia plan,String direccion,LocalDate ultima_cobrada,Provincias prov){
+        super( codigo, plan, direccion, ultima_cobrada, prov);
+    }
+
+    @Override
+    public double calcularValorPagar(LocalDateTime fechaAccion) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
