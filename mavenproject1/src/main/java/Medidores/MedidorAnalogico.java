ced27291eb7a726316f6d638f6ad72e8d5e8c03e
Alexorrala <91976823+AlexOrrala@users.noreply.github.com>
2021-12-02 10:39:39 -0500

    medidor analogico

--- mavenproject1/src/main/java/Medidores/MedidorAnalogico.java
+++ mavenproject1/src/main/java/Medidores/MedidorAnalogico.java
@@ -11,24 +11,10 @@ import java.time.LocalDateTime;
  * @author samu_
  */
 public class MedidorAnalogico extends Medidor {
-    private LocalDateTime ultimamed;
-    private int Lecturaa;
+    private LocalDateTime FechaToma;
+    private double kilovatios;
     public MedidorAnalogico(LocalDateTime ultimamed,int Lecturaa){
         super();
-        this.ultimamed=ultimamed;
-        this.Lecturaa=Lecturaa;
-    }
-    public LocalDateTime getUltimamed(){
-        return ultimamed;
-    }
-    public int getLectuaraa(){
-        return Lecturaa;
-    }
-    public void setUltimamed(LocalDateTime ultimamed ){
-        this.ultimamed=ultimamed;
-    }
-    public void setLecturaa(int Lecturaa){
-        this.Lecturaa=Lecturaa;
     }
 
     @Override
