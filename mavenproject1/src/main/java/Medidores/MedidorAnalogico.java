5dfddee2079c47392ac9c6e22f0e291664cb8e6e
Jslucin <93801127+Jslucin@users.noreply.github.com>
2021-12-02 21:44:17 -0500

    Medidor Analogico terminado

--- mavenproject1/src/main/java/Medidores/MedidorAnalogico.java
+++ mavenproject1/src/main/java/Medidores/MedidorAnalogico.java
@@ -16,13 +16,19 @@ import java.time.LocalDateTime;
 public class MedidorAnalogico extends Medidor {
     private LocalDateTime FechaToma;
     private double kilovatios;
-    public MedidorAnalogico(String codigo,PlanEnergia plan,String direccion,LocalDate ultima_cobrada,Provincias prov){
-        super( codigo, plan, direccion, ultima_cobrada, prov);
+    public MedidorAnalogico(PlanEnergia plan,String direccion,LocalDateTime FechaToma, double kilovatios){
+        super( plan, direccion);
+        this.FechaToma=FechaToma;
+        this.kilovatios=kilovatios;
     }
 
     @Override
     public double calcularValorPagar(LocalDateTime fechaAccion) {
-       return 0;
+       Lectura anadida=new Lectura(FechaToma,kilovatios);
+       lectura.add(anadida);
+       ultima_cobrada=fechaAccion;
+       double costo=kilovatios*plan.getCostoK();
+       return costo;
     }
     
     
