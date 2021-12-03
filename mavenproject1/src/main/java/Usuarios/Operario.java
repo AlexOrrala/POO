dd2c63ef4a1c64bacef0b584f0a9bc1261616b0e
Alexorrala <91976823+AlexOrrala@users.noreply.github.com>
2021-12-03 11:08:58 -0500

    Operario

--- mavenproject1/src/main/java/Usuarios/Operario.java
+++ mavenproject1/src/main/java/Usuarios/Operario.java
@@ -6,6 +6,12 @@
 
 package Usuarios;
 
+import Medidores.Lectura;
+import Medidores.Medidor;
+import Medidores.MedidorDigital;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
 /**
  * 
  * @author Sammy Guergachi <sguergachi at gmail.com>
@@ -15,5 +21,18 @@ public class Operario extends Usuario{
     public Operario(String nombre, String contrasenia) {
         super(nombre, contrasenia);
     }
-
+    public void registrarmedicion(String codigo){
+        Scanner sc = new Scanner(System.in);
+        Lectura l; 
+        Double kv;
+        for(MedidorDigital c : MedidorDigital.getListasmedidor()){
+            if(c.getCodigo()== codigo){
+                System.out.println("Ingrese lectura actual del medidor:"+codigo);
+                kv = sc.nextDouble();
+                l = new Lectura(LocalDateTime.now(), kv);
+                l.setOperario(this);
+                c.agregarlectura(l);
+            }
+        }
+    }
 }
