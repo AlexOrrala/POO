3fac1a0ca181916876887b04ba76e30a0a90d473
Jslucin <93801127+Jslucin@users.noreply.github.com>
2021-12-02 22:21:31 -0500

    MedidorDigital

--- mavenproject1/src/main/java/Medidores/MedidorDigital.java
+++ mavenproject1/src/main/java/Medidores/MedidorDigital.java
@@ -5,10 +5,14 @@
 
 package Medidores;
 
+import Plan.HorarioPico;
 import Plan.PlanEnergia;
 import Plan.Provincias;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Random;
 
 /**
  * 
@@ -17,12 +21,42 @@ import java.time.LocalDateTime;
 public class MedidorDigital extends Medidor{
     private LocalDateTime FechaToma;
     private double kilovatios;
-    public MedidorDigital(String codigo,PlanEnergia plan,String direccion,LocalDate ultima_cobrada,Provincias prov){
-        super( codigo, plan, direccion, ultima_cobrada, prov);
+    public MedidorDigital(String direccion, PlanEnergia plan){
+        super(plan, direccion);
+        Timer timer= new Timer();
+        TimerTask anadir= new TimerTask() {
+            @Override
+            public void run() {
+                FechaToma=LocalDateTime.now();
+                kilovatios=Math.floor(Math.random()*10);
+                lectura.add(new Lectura(FechaToma,kilovatios));
+            }
+            
+        };
+        timer.schedule(anadir,0,1800000);
     }
+    public LocalDateTime getFechaToma(){
+        return FechaToma;
+    }
+    public double getKilovatios(){
+        return kilovatios;
+    }
+    
 
     @Override
     public double calcularValorPagar(LocalDateTime fechaAccion) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        double total=0;
+        for (Lectura i: lectura){
+            for(HorarioPico l:plan.getPicos()){
+                if (l.getHorarioinicio().isBefore(i.getFechaToma().toLocalTime()) & l.getHorafin().isAfter(i.getFechaToma().toLocalTime())){
+                    total=total+i.getKilovatios()*plan.getCostoK()*2;
+                }
+                else{
+                    total=total+i.getKilovatios()*plan.getCostoK();
+                }
+                    
+            }
+        }
+        return total;
     }
 }
