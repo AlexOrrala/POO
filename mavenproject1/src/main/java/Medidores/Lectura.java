72faad3e96d125808cd4dcc62b0aff054f404066
jucadura <93133974+jucadura@users.noreply.github.com>
2021-12-03 06:39:34 -0500

    Update Lectura.java

--- mavenproject1/src/main/java/Medidores/Lectura.java
+++ mavenproject1/src/main/java/Medidores/Lectura.java
@@ -6,17 +6,17 @@ import java.time.LocalDateTime;
  * 
  * @author Sammy Guergachi <sguergachi at gmail.com>
  */
-public class Lectura {
-    private LocalDateTime FechaToma;
+public class Lectura{
+    private Local Datetime FechaToma;
     private double kilovatios;
-    public Lectura(LocalDateTime FechaToma,double kilovatios){
+    public Lectura(LocalDateTime FechaTOma, double kilovatios){
         this.FechaToma=FechaToma;
         this.kilovatios=kilovatios;
     }
-    public LocalDateTime getFechaToma(){
+    public LocalDateTIm getFechaToma(){
         return FechaToma;
     }
-    public double getKilovatios(){
+    public double getkilovatios(){
         return kilovatios;
     }
 }
